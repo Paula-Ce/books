5ae0d01f4aef5ba522c3b3951723a15cc68561ba
Paula Celemín <p.celemin@hotmail.com>
2023-04-28 20:57:59 +0200

    routes update y delete

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, g, jasonify
+from flask import Flask, g, jsonify, request
 import psycopg2
 
-app = Flask (__name__)
+app = Flask (__name__) # Esto lo utiliza FLASK para saber dónde está ubicado nuestro archivo principal. Sería lo mismo que pasarle "__main__"
 
 # Configuración de la conexión a la base de datos
 
@@ -60,9 +60,79 @@ def get_books():
     books = cursor.fetchall() 
     cursor.close()
 
-    return jasonify(books)
+    return jsonify(books)
+
+
+# 2. Ruta para añdir 1 libro a la BBDD
+
+@app.route("/resources/book/add", methods = ["POST"])
+def add_book():
+       
+    # Obtener datos del cuerpo de la petición
+    book = request.get_json()
+
+    # Obtenemos los datos del libro a través de los parámetros de la petición
+    # author = request.args.get ("author")
+
+    # Almacenar los datos en variables
+    author = book["author"]
+    year = book["year"]
+    title = book["title"]
+    description = book["description"]
+    id_book = book ["id"]
+
+    # Crear la conexión a la base de datos
+    conn = get_db()
+    cursor = conn.cursor ()
+    cursor.execute("INSERT INTO books_table (id, author, year, title, description) VALUES (%s, %s, %s, %s, %s)", (id_book, author, year, title, description))
+    conn.commit()
+    cursor.close()
+
+    return jsonify ({"message" : "El libro ha sido añadido correctamente"})
+
+
+# 3. Ruta para eliminar un libro por su ID
+
+@app.route("/resources/book/delete/<int:id>", methods = ["DELETE"])
+def delete_book(id):
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor .execute("DELETE FROM books_table WHERE id = %s", (id,)) # Se pone coma detrás de "id" para que entienda que es un SET y no unos paréntesis normales.
+    conn.commit()
+    cursor.close()
+
+    return jsonify ({"message": "El libro ha sido eliminado correctamente."})
+
+
+# 3. Ruta para actualizar un libro
+
+@app.route("/resources/book/update", methods = ["PUT"])
+def update_book():
+
+    # Almacenar los datos en las variables. Esta vez en lugar de obtenerlo por el body vamos a obtenerlo por parámetros
+    # Si los parámetros son cortos, se puede hacer así. 
+    # Si son larguísimos, al meterlo por parámetros se pone todo en la ruta, por lo que es un churro. En ese caso mejor por el body.
+
+    title = request.args["title"]
+    year = request.args["year"]
+
+    # Conectar con la base de datos
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor .execute("UPDATE books_table SET year = %s WHERE title = %s", (year, title)) # Ojo que mantengamos el dato "title" y cambiemos "year" y no al revés.
+    conn.commit()
+    cursor.close()
+
+    return jsonify ({"message": "El libro ha sido actualizado correctamente."})
 
 
 # Ejecutar la aplicación
+
+# Es una buena práctica que el código que se ejecuta al ejecutar el archivo sea el siguiente: "if__name__ == "__main__"
+# Esto es para que el servidor sólo se ejecute cuando se ejecute el archivo main.py, y no cuando se importe desde otro archivo.
+
+# Si tu archivo Python es sólo un script simple que se ejecuta de manera directa, y no tiene la intención de ser reutilizado como un módulo
+# en otro lugar, entonces no necesitas nacesariamente utilizarlo.
+
 if __name__ == '__main__':
     app.run(debug = True)
